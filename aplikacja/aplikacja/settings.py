b03b56330051c8482e1d4ecf410cefcdb8ac42bc
DominikSzczygielski <temptemptemptemp1@o2.pl>
2025-02-24 17:28:13 +0100

    wersja 3

--- aplikacja/aplikacja/settings.py
+++ aplikacja/aplikacja/settings.py
@@ -55,8 +55,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 ROOT_URLCONF = 'aplikacja.urls'
 
 TEMPLATES = [
